5098eb920fa9afd49d3e47f503b4acc66595744c
SashaKorobkina <s.dekaiper@gmail.com>
2023-01-24 11:38:35 +0000

    added basic functions

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -62,3 +62,97 @@ let score = 0;
 let answers = [];
 
 
+function toggleVisibility(elementId) {
+	document.getElementById(elementId).classList.toggle('hide');
+};
+
+function submitScore() {
+	const initials = document.getElementById(initialsId).value;
+
+	const savedAnswers = JSON.parse(localStorage.getItem(storageKey)) || [];
+
+	savedAnswers.push({ initials, score, questionsCount: questions.length });
+	localStorage.setItem(storageKey, JSON.stringify(savedAnswers));
+
+	answers = [];
+	score = 0;
+
+	toggleVisibility(endScreenId);
+	toggleVisibility(startScreenId);
+	document.getElementById(timeId).innerText = quizTime;
+	document.getElementById(initialsId).value = '';
+};
+
+function finishQuiz() {
+	clearInterval(interval);
+	quizTime = 90;
+	currentQuestion = 0;
+
+	toggleVisibility(questionsId);
+	toggleVisibility(endScreenId);
+
+	answers.forEach(function(isAnswerCorrect) {
+		 if (isAnswerCorrect) {
+			  score = score + 1;
+		 }
+	});
+
+	document.getElementById(scoreId).innerHTML = score + ' / ' + questions.length;
+};
+
+function startTimer() {
+	interval = setInterval(function() {
+		 if (quizTime <= 0) {
+			  finishQuiz();
+		 } else {
+			  quizTime = quizTime - 1;
+		 }
+		 
+		 document.getElementById(timeId).innerText = quizTime;
+	}, 1000);
+};
+
+function submitAnswer(event) { 
+	const userAnswer = event.target.innerText;
+	const isAnswerCorrect = userAnswer === questions[currentQuestion].answer;
+	answers[currentQuestion] = isAnswerCorrect;
+
+	if (!isAnswerCorrect) {
+		 quizTime = quizTime - 10;
+	}
+
+	if (currentQuestion === questions.length - 1) {
+		 finishQuiz();
+	} else {
+		 currentQuestion = currentQuestion + 1;
+		 renderQuestion();
+	}
+};
+
+function renderQuestion() {
+	const question = questions[currentQuestion];
+
+	document.getElementById(questionsTitleId).innerHTML = question.question;
+	const choices = document.getElementById(choicesId);
+	choices.innerHTML = '';
+	
+	question.options.forEach(function(option) {
+		 const optionButton = document.createElement('button');
+		 optionButton.innerText = option;
+		 optionButton.addEventListener('click', submitAnswer);
+
+		 choices.appendChild(optionButton);
+	});  
+};
+
+function start() {
+	toggleVisibility(startScreenId);
+	toggleVisibility(questionsId);
+	document.getElementById(timeId).innerText = quizTime;
+	
+	startTimer();
+	renderQuestion();
+};
+
+document.getElementById('start').addEventListener('click', start);
+document.getElementById('submit').addEventListener('click', submitScore);
