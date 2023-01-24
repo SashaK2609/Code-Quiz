d2f3cf6d8a0fdc8b9a31d923b6646291d434b132
SashaKorobkina <s.dekaiper@gmail.com>
2023-01-24 11:46:02 +0000

    added comments

--- assets/js/hs.js
+++ assets/js/hs.js
@@ -1,3 +1,4 @@
+		//initialize variables
 		const highscoresId = 'highscores';
       const clearButtonId = 'clear';
       const storageKey = 'answers';
@@ -8,13 +9,16 @@
       };
 		
       function renderHighscores() {
+			//get informationg from local storage after submitting
         const data = localStorage.getItem(storageKey);
 
+		  //parse data into data array or use empty array
         const scores = JSON.parse(data) || [];
 
         const list = document.getElementById(highscoresId);
         list.innerHTML = '';
 
+		  //create a score list with initials and score
         scores.forEach(function(score) {
           const listItem = document.createElement('li');
           listItem.innerText = score.initials + ' - ' + score.score + ' / ' + score.questionsCount;
@@ -23,5 +27,6 @@
         });
       };
 
+		//clear highscores button clears all the list
       document.getElementById(clearButtonId).addEventListener('click', clear);
       renderHighscores();
